acb3b0d563f23c944ef2e3a4d5fedf88ebdd1914
Ayan Khan <impracticalayan@gmail.com>
2023-11-23 21:04:48 +0530

    Day:01 completed

---
+++ Lectures/Lecture-02/Code/file-01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // DATA TYPES
+    int num = 5;
+    char name = 'A';
+    float decimalNumber = 2.34;
+    char letter = 98;
+    cout<<"letter is "<<letter<<endl;
+}

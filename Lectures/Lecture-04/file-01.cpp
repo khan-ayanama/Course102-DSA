a74208e2aa961a8abd1748ef923ffe0fe324d164
Ayan Khan <impracticalayan@gmail.com>
2023-11-25 12:53:37 +0530

    Lecture-04

---
+++ Lectures/Lecture-04/file-01.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // PATTERN PRINTING
+
+    // 1. Pattern-01
+    // int num = 4;
+    // for (int i = 1; i <= num; i++)
+    // {
+    //     for (int j = 1; j <= num; j++)
+    //     {
+    //         cout<<j<<" ";
+    //     }cout<<endl;
+        
+    // }
+
+    // Pattern-02
+    // int num = 3;
+    // int count = 1;
+    // for (int i = 0; i < num; i++)
+    // {
+    //     for (int j = 0; j < num; j++)
+    //     {
+    //         cout<<count<<" ";
+    //         count++;
+    //     }cout<<endl;
+        
+    // }
+
+    // Pattern-03
+    // int num = 3;
+    // int count = 0;
+    // for (int i = 0; i < num; i++)
+    // {
+    //     for (int j = 0; j < num; j++)
+    //     {
+    //         cout<<(num*num) - count<<" ";
+    //         count++;
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    
+}
